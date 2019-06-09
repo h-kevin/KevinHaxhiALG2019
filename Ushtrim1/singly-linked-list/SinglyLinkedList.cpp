28c77c1447c03deb846081816aa7c9cf415743eb
Kevin <kevin.haxhi@outlook.com>
2019-06-10 01:07:36 +0200

    finished SinglyLinkedList class

--- Ushtrim1/singly-linked-list/SinglyLinkedList.cpp
+++ Ushtrim1/singly-linked-list/SinglyLinkedList.cpp
@@ -16,6 +16,7 @@ class SinglyLinkedList {
 private:
 
     node *head, *tail;
+    int counter;
 
 public:
 
@@ -23,6 +24,7 @@ public:
 
         this->head = NULL;
         this->tail = NULL;
+        this->counter = 0;
     }
 
     void add(string w) {
@@ -30,6 +32,7 @@ public:
         node *tmp = new node;
         tmp->data = w;
         tmp->next = w;
+        this->counter++;
 
         if (this->head == NULL && this->tail == NULL) {
 
@@ -57,12 +60,14 @@ public:
                     d = tmp->data;
                     this->head = this->head->next;
                     tmp = NULL;
+                    this->counter--;
                     return d;
                 } else {
 
                     d = tmp->data;
                     p->next = tmp->next;
                     tmp = NULL;
+                    this->counter--;
                     return d;
                 }
             }
@@ -73,4 +78,52 @@ public:
 
         return NULL;
     }
-}
+
+    int find(string w) {
+
+        if (this->head == NULL)
+            return -1;
+        
+        node *tmp = this->head;
+        int c = 1;
+
+        while (tmp != NULL && tmp->data != w) {
+
+            tmp = tmp->next;
+            c++;
+        }
+
+        if (tmp == NULL)
+            return -1;
+        else
+            return c;
+    }
+
+    int getNodesNumber() {
+
+        return this->counter;
+    }
+
+    void print() {
+
+        if (this->head == NULL);
+            cout << "NULL";
+
+        node *tmp = this->head;
+        int c = 1;
+
+        while (tmp != NULL) {
+
+            cout << tmp->data;
+
+            if (c % 5 == 0)
+                cout << endl << "-> ";
+            else
+                cout << " -> ";
+
+            tmp = tmp->next;
+        }
+
+        cout << "NULL";
+    }
+} // end class SinglyLinkedList
