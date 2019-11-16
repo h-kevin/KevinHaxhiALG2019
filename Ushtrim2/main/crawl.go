abb651580c1ffb45754d5ff3d613b8b9de612527
Kevin <kevin.haxhi@outlook.com>
2019-11-16 17:43:29 +0100

    changed order of functions

--- Ushtrim2/main/crawl.go
+++ Ushtrim2/main/crawl.go
@@ -17,40 +17,6 @@ func usage() {
 	os.Exit(2)
 }
 
-func main() {
-
-	flag.Usage = usage
-	flag.Parse()
-
-	args := flag.Args()
-	fmt.Println(args)
-	if len(args) < 1 {
-		usage()
-		fmt.Println("Please specify start page")
-		os.Exit(1)
-	}
-
-	queue := make(chan string)
-	filteredQueue := make(chan string)
-
-	go func() { queue <- args[0] }()
-	go filterQueue(queue, filteredQueue)
-
-	// introduce a bool channel to synchronize execution of concurrently running crawlers
-	done := make(chan bool)
-
-	// pull from the filtered queue, add to the unfiltered queue
-	for i := 0; i < 5; i++ {
-		go func() {
-			for uri := range filteredQueue {
-				enqueue(uri, queue)
-			}
-			done <- true
-		}()
-	}
-	<-done
-}
-
 func filterQueue(in chan string, out chan string) {
 	var seen = make(map[string]bool)
 	for val := range in {
@@ -97,3 +63,39 @@ func fixURL(href, base string) string {
 	uri = baseURL.ResolveReference(uri)
 	return uri.String()
 }
+
+func main() {
+
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		usage()
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
+
+	queue := make(chan string)
+	filteredQueue := make(chan string)
+
+	go func() { queue <- args[0] }()
+	go filterQueue(queue, filteredQueue)
+
+	// introduce a bool channel to synchronize execution of concurrently running crawlers
+	done := make(chan bool)
+
+	// pull from the filtered queue, add to the unfiltered queue
+	for i := 0; i < 5; i++ {
+		go func() {
+			for uri := range filteredQueue {
+				enqueue(uri, queue)
+			}
+			done <- true
+		}()
+	}
+	<-done
+}
+
+
